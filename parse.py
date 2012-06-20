d929e30568679ac01bd724c92227a6e35b3795bb
Mao Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2012-06-20 21:51:12 +0900

    restruct directories

--- parse.py
+++
@@ -1,6 +0,0 @@
-from xml.etree import ElementTree
-filename = 'pmc_sample.xml'
-tree = ElementTree.parse(filename)
-abstract = tree.getroot().findall('.//abstract/p')
-for p in abstract:
-  print p.text
