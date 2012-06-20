1049ba611c118adee74e1f1cc19e0aea3c0acc27
Kou AMANO <amano.au1@gmail.com>
2012-06-20 19:33:57 +0900

    first freq_e

---
+++ freq_e.py
@@ -0,0 +1,16 @@
+#!/opt/local/bin/python2.7
+#-*- coding:utf-8 -*-
+# freq_e.py
+from ngram import ngram
+from nlist import nlist
+def freq_e(el,n,strs):
+  source = nlist(strs,n)
+  i = 0
+  for term in source:
+    if term == el:
+      i = i + 1
+  return i
+
+if __name__ == "__main__":
+  strs = ['ghram','n-ghramgh','ghram-positive']
+  print freq_e('gh',2,strs)
